2a1679d72e28589c725377aa0c4e3e02c8ee20c9
Christopher Pietsch <cpietsch@gmail.com>
2017-09-05 13:52:00 +0200

    Updates

--- static/js/manifest.ff83fb67f863ca381c35.js
+++ static/js/manifest.ff83fb67f863ca381c35.js
@@ -141,11 +141,11 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = ",/";
+/******/ 	__webpack_require__.p = "./";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=manifest.21b906e416f17d09f1c2.js.map
+//# sourceMappingURL=manifest.ff83fb67f863ca381c35.js.map
